688f4070b08ce068df70ad3629a05ee264a2472f
HCEhlers <holgerehlers@hotmail.com>
2021-03-18 16:44:47 +0100

    adding comment to the doit R file

--- R/00_doit.R
+++ R/00_doit.R
@@ -1,4 +1,5 @@
 # Run all scripts ---------------------------------------------------------
+# This will run all the r files
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
